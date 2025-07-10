8f36f2689d86a93436997f0283a5a9ebd85ac253
Sreeja-1408 <sreejalabhishettty@gmail.com>
2025-07-10 13:01:56 +0530

    Changes

--- App.js
+++ App.js
@@ -50,17 +50,17 @@ import ScanExchangeOrders from "./src/Exchange/ScanExchangeOrders";
 import WeeklyOrders from "./src/SalesOrders/WeeklyOrders";
 import AllCategories from "./src/SalesOrders/AllCategories";
 import WeeklyDashboard from "./src/SalesOrders/WeeklyDashboard";
-import InventoryScreen from "./src/SalesOrders/InventoryScreen"
-import MarketOrdersScreen from "./src/SalesOrders/MarketOrdersScreen"
-import MarketVisitsScreen from "./src/SalesOrders/MarketVisitsScreen"
+import InventoryScreen from "./src/SalesOrders/InventoryScreen";
+import MarketOrdersScreen from "./src/SalesOrders/MarketOrdersScreen";
+import MarketVisitsScreen from "./src/SalesOrders/MarketVisitsScreen";
 import MarketListItems from "./src/SalesOrders/MarketListItems";
-import RecordInventoryScreen from "./src/SalesOrders/RecordInventoryScreen"
-import CreateOrderScreen from "./src/SalesOrders/CreateOrderScreen"
-import AddMarket from "./src/SalesOrders/AddMarket"
+import RecordInventoryScreen from "./src/SalesOrders/RecordInventoryScreen";
+import CreateOrderScreen from "./src/SalesOrders/CreateOrderScreen";
+import AddMarket from "./src/SalesOrders/AddMarket";
 
 import PlaceOrder from "./src/SalesOrders/Place Orders/PlaceOrder";
 import ProceedtoCheckout from "./src/SalesOrders/Place Orders/ProceedtoCheckout";
-import Address from "./src/SalesOrders/Place Orders/Address"
+import Address from "./src/SalesOrders/Place Orders/Address";
 
 import MarketOrders from "./src/SalesOrders/MarketOrders";
 
@@ -136,18 +136,21 @@ export default function App() {
         /> */}
           {/* <Stack.Screen name="Orders" component={Main} /> */}
           <Stack.Screen
-  name="Orders"
-  component={Main}
-  initialParams={{ isTestOrder: false }}   // fallback for first load
-  options={({ route }) => ({
-    title: route.params?.isTestOrder ? 'Test Orders' : 'Orders',
-  })}
-/>
+            name="Orders"
+            component={Main}
+            initialParams={{ isTestOrder: false }} // fallback for first load
+            options={({ route }) => ({
+              title: route.params?.isTestOrder ? "Test Orders" : "Orders",
+            })}
+          />
           <Stack.Screen name="Exchange" component={ExchangeMain} />
           <Stack.Screen name="Products" component={Items} />
-          <Stack.Screen name="Orders Report" component={OrdersReport}/>
-          <Stack.Screen name="Orders Stats" component={OrderStats}/>
-          <Stack.Screen name="Pincode wise orders" component={PinCodeWiseOrders}/>
+          <Stack.Screen name="Orders Report" component={OrdersReport} />
+          <Stack.Screen name="Orders Stats" component={OrderStats} />
+          <Stack.Screen
+            name="Pincode wise orders"
+            component={PinCodeWiseOrders}
+          />
           <Stack.Screen name="Categories" component={CategoriesScreen} />
           <Stack.Screen name="Delivery Boys" component={DeliveryBoys} />
           <Stack.Screen
@@ -167,27 +170,25 @@ export default function App() {
               headerRight: () => <Logout />,
               headerLeft: () => <></>,
             }}/> */}
-            <Stack.Screen
-  name="Store Details"
-  component={StoreDetailsScreen}
-  options={{
-    headerShown: true,
-    headerRight: () => <Logout />,
-    headerLeft: () => <></>,
-    headerStyle: {
-      backgroundColor: '#3B82F6', // or any visible color
-    },
-    headerTintColor: 'white', // icon/text color
-  }}
-/>
+          <Stack.Screen
+            name="Store Details"
+            component={StoreDetailsScreen}
+            options={{
+              headerShown: true,
+              headerRight: () => <Logout />,
+              headerLeft: () => <></>,
+              headerStyle: {
+                backgroundColor: "#3B82F6", // or any visible color
+              },
+              headerTintColor: "white", // icon/text color
+            }}
+          />
 
-          <Stack.Screen name="Store Orders" component={PlacedOrdersScreen}/>
-          <Stack.Screen name="Checkout" component={Checkout}  />
+          <Stack.Screen name="Store Orders" component={PlacedOrdersScreen} />
+          <Stack.Screen name="Checkout" component={Checkout} />
           <Stack.Screen name="Cart" component={CartScreen} />
           <Stack.Screen name="Dashboard" component={DashboardScreen} />
 
-           
-         
           <Stack.Screen
             name="OfferImagesScreen"
             component={OfferImagesScreen}
@@ -216,61 +217,64 @@ export default function App() {
               headerRight: () => <Logout />,
               headerLeft: () => <></>,
             }}/> */}
-            <Stack.Screen
-  name="Sales Dashboard"
-  component={WeeklyDashboard}
-  options={{
-    headerShown: true,
-    headerRight: () => <Logout />,
-    headerLeft: () => <></>,
-    headerStyle: {
-      backgroundColor: '#3B82F6', // or any visible color
-    },
-    headerTintColor: 'white', // icon/text color
-  }}
-/>
+          <Stack.Screen
+            name="Sales Dashboard"
+            component={WeeklyDashboard}
+            options={{
+              headerShown: true,
+              headerRight: () => <Logout />,
+              headerLeft: () => <></>,
+              headerStyle: {
+                backgroundColor: "#3B82F6", // or any visible color
+              },
+              headerTintColor: "white", // icon/text color
+            }}
+          />
 
-      <Stack.Screen name="Inventory" component={InventoryScreen} />
-      <Stack.Screen name="MarketOrders" component={MarketOrdersScreen} />
-      {/* <Stack.Screen name="Market Visits" component={MarketVisitsScreen} options={{
+          <Stack.Screen name="Inventory" component={InventoryScreen} />
+          <Stack.Screen name="MarketOrders" component={MarketOrdersScreen} />
+          {/* <Stack.Screen name="Market Visits" component={MarketVisitsScreen} options={{
               headerShown: true,
               headerRight: () => <Logout />,
               headerLeft: () => <></>,
             }}
              /> */}
-             <Stack.Screen
-  name="Market Visits"
-  component={MarketVisitsScreen}
-  options={{
-    headerShown: true,
-    headerRight: () => <Logout />,
-    headerLeft: () => <></>,
-    headerStyle: {
-      backgroundColor: '#3d2a71', // or any visible color
-    },
-    headerTintColor: 'white', // icon/text color
-  }}
-/>
+          <Stack.Screen
+            name="Market Visits"
+            component={MarketVisitsScreen}
+            options={{
+              headerShown: true,
+              headerRight: () => <Logout />,
+              headerLeft: () => <></>,
+              headerStyle: {
+                backgroundColor: "#3d2a71", // or any visible color
+              },
+              headerTintColor: "white", // icon/text color
+            }}
+          />
 
-      <Stack.Screen name="Market List Items" component={MarketListItems}/>
-      <Stack.Screen 
-        name="Record Inventory" 
-        component={RecordInventoryScreen} 
-        options={{ title: "Record Inventory Movement" }}
-      />
-      <Stack.Screen 
-        name="CreateOrder" 
-        component={CreateOrderScreen} 
-        options={{ title: "Create New Order" }}
-      />
-      <Stack.Screen 
-        name="Add Market" 
-        component={AddMarket} 
-        options={{ title: "Schedule Market Visit" }}
-      />
-      <Stack.Screen name="Place Order" component={PlaceOrder}/>
-          <Stack.Screen name="Proceed to Checkout" component={ProceedtoCheckout}/>
-          <Stack.Screen name="Address Book" component={Address}/>
+          <Stack.Screen name="Market List Items" component={MarketListItems} />
+          <Stack.Screen
+            name="Record Inventory"
+            component={RecordInventoryScreen}
+            options={{ title: "Record Inventory Movement" }}
+          />
+          <Stack.Screen
+            name="CreateOrder"
+            component={CreateOrderScreen}
+            options={{ title: "Create New Order" }}
+          />
+          <Stack.Screen
+            name="Add Market"
+            component={AddMarket}
+            options={{ title: "Schedule Market Visit" }}
+          />
+          <Stack.Screen name="Place Order" component={PlaceOrder} />
+          <Stack.Screen
+            name="Proceed to Checkout"
+            component={ProceedtoCheckout}
+          />
+          <Stack.Screen name="Address Book" component={Address} />
 
           <Stack.Screen name="Market Orders" component={MarketOrders} />
           {/* Add more screens as needed */}
@@ -280,4 +284,3 @@ export default function App() {
     </Provider>
   );
 }
-
